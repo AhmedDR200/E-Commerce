420f2fd005ad07e8d7a907b0576bb2619a2a8a60
AhmedDR200 <alshwwhy212@gmail.com>
2023-10-30 23:28:19 +0200

    Create views, Urls for Category Model

--- product/views.py
+++ product/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
 
-# Create your views here.
+
+class CategoryView(viewsets.ViewSet):
+    queryset = Category.objects.all()
+
+    def list(self, request):
+        serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
